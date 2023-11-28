98623935adecb85b420466ddeb1545357fd0e35e
wilianbot <wiliamrobaldossantos1@gmail.com>
2023-11-28 19:54:00 -0300

    True and false e conversoes

---
+++ truthy-falsy/truthy-falsy.js
@@ -0,0 +1,24 @@
+// boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// nul ==> vazio ou nada
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero) 
+console.log(typeof texto)
