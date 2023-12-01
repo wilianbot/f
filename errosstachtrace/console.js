eae95eb34b19baa08d53a7dbb67d6e8af0f5643d
wilianbot <wiliamrobaldossantos1@gmail.com>
2023-11-30 22:08:41 -0300

    erros

---
+++ errosstachtrace/console.js
@@ -0,0 +1,8 @@
+const minhaVar = true;
+
+// tratamento de erro
+
+console.error("Deu erro!");
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
---
+++ errosstachtrace/erros.js
@@ -0,0 +1,5 @@
+// Erro
+const numero;
+
+// Funciona
+const numero2 = 0;
