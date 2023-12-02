d283b8411fd34720f1b0c842e6633e80c423b19b
wilianbot <wiliamrobaldossantos1@gmail.com>
2023-12-02 13:16:16 -0300

    comparação literal e ternario

---
+++ comparadorTernario/ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 18;
+
+//  if (idadeCliente >= idadeMinima) {
+//     console.log("Cerveja");
+//  } else {
+//     console.log("suco");
+//  }
+
+            // condição                    // true   // false
+ console.log(idadeCliente >= idadeMinima ? "ceveja" : "suco")
