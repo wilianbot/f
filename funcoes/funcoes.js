5a89a59ecfbe112480b3f06095ca0ae3a74c5342
wilianbot <wiliamrobaldossantos1@gmail.com>
2023-12-03 15:42:27 -0300

    Funções

---
+++ funcoes/funcoes.js
@@ -0,0 +1,22 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) declara a função
+
+// function imprimeTexto(texto) {
+//     console.log(texto)
+// }
+
+// 2) executa a  função (1 ou + vezes)
+
+// imprimeTexto("oi");
+// imprimeTexto("Outro texto");
+
+// tres formas de escrever funções
+
+function soma(){
+    return 2 + 2;
+}
+
+console.log(soma())
---
+++ funcoes/parametros.js
@@ -0,0 +1,23 @@
+// parâmetros de função
+
+function soma(a, b){
+    return a + b;
+}
+
+console.log(soma(4, 5))
+
+// parâmentros x argumentos
+
+// ordem dos parâmetros
+
+function nomeIdade(nome, idade) {
+    return `meu nome é ${nome} e minha idade é ${idade}`
+}
+
+// console.log(nomeIdade("Wilian", 22))
+
+function multiplca(numero1, numero2){
+    return numero1 * numero2;
+}
+
+console.log(multiplca(soma(5, 3), soma(2, 6)));
