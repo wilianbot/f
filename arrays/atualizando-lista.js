0a24bdce0188ae6e682a97945b3e923f7aca77a7
wilianbot <wiliamrobaldossantos1@gmail.com>
2023-12-16 14:11:25 -0300

    metodos arrays

---
+++ arrays/atualizando-lista.js
@@ -0,0 +1,13 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Majorie", "Leo"];
+
+nomes.splice(1, 2, "Gabriel");
+nomes.push("Wilian");
+
+console.log(nomes);
+
+animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(1,0,'🐠')
+animaisDoAquario.splice(3,2,'🐟')
+
+console.log(animaisDoAquario)
